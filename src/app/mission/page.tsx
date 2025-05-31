3b747d0dd846f2b66185673d2486bcf6013029f6
almas-josephine <almas.josephine.placides@gmail.com>
2025-05-31 18:56:53 -0400

    second commit

--- src/app/mission/page.tsx
+++
@@ -1,8 +0,0 @@
-import '@/app/styles/globals.css';
-import '@/app/layout';
-function Missionpage() {
-    return (
-        <h2>Hello</h2>
-    )
-}
-export default Missionpage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,8 @@ import React from 'react';
 
 function MyApp() {
   return (
-    <div className="container max-w-4xl px-4 py-8" style={{ transform: 'translate(150px, 30px)', position: 'sticky', top: '0', zIndex: '-1' }}>
-
-      <script src="https://static.elfsight.com/platform/platform.js" async></script>
-      <div className="elfsight-app-d1fc404b-4dbb-496c-a420-27e007bed2f8" data-elfsight-app-lazy></div>
-
-    </div >
+    <h2>Hello</h2>
   );
 }
 
-export default MyApp  
+export default MyApp
