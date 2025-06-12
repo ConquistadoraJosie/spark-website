f8f2621931c17f43401354c5a3bb60e86cf245e8
almas-josephine <almas.josephine.placides@gmail.com>
2025-06-12 13:22:03 -0400

    morning updates

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,12 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./your-files/**/*.{html,js}'],
-    // other stuff...
-
-    daisyui: {
-
-        themes: ['light', 'dark', 'cupcake'],
-
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {},
     },
-
-    plugins: [require("daisyui")],
-}
+    plugins: [
+        require('daisyui'), // Add this line
+    ],
+    // daisyUI config (optional - will be set to default if no config is provided)
+    daisyui: {
+        themes: ["light", "dark", "cupcake"], // Add the themes you want to use
+        // other daisyUI configuration options...
+    },
+};
