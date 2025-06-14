ac8482a10e3b8a34a236b65f09f65fc2c87ba688
almas-josephine <almas.josephine.placides@gmail.com>
2025-06-14 17:36:26 -0400

    Navigation bar and body are now responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-
 import React from "react";
+import '@/app/layout.tsx'
+import '@/app/styles/globals.css'
+import missionPage from '@/app/Images/missionpage.png'
+import Image from "next/image";
 
 export default function Page() {
   return (
 
-    <h1 className="top-20px">Hello, world!</h1>
-
+    <div className="flex-container">
+      <div className="flex-item-left">Image Carosel TBD</div>
+      <div className="flex-item-right">
+        <Image src={missionPage} alt="mission" className="responsive" /></div>
+    </div>
   );
 }
